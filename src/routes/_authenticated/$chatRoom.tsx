18b1417d198d1804f6178f6ab0d5e16641e1983a
Aytuhan Kazandırmak <aytuhann@gmail.com>
2025-12-16 09:38:41 +0300

    update for iphone devices

--- src/routes/_authenticated/$chatRoom.tsx
+++ src/routes/_authenticated/$chatRoom.tsx
@@ -55,7 +55,7 @@ function RouteComponent() {
   }, [messages]);
 
   return (
-    <div className="flex w-full flex-col  h-dvh">
+    <div className="flex w-full flex-col  h-screen">
       <div>
         <ChatHeader chat={chatDetails} />
       </div>
